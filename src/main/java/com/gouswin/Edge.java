2e846e069cc613bd5555100bf67e7edbc8b7a4cb
ShovelTime <gousaid67@gmail.com>
2023-03-28 17:10:30 +0200

    Edge now has travelTypes.

--- src/main/java/com/gouswin/Edge.java
+++ src/main/java/com/gouswin/Edge.java
@@ -4,15 +4,18 @@ public class Edge {
 
     private int weight;
 
-    private String name;
-    private Node origin;
-    private Node destination;
+    private final String name;
+    private final Node origin;
+    private final Node destination;
 
-   public Edge(String name, Node origin, Node destination, int weight) {
+    private final String travelType;
+
+   public Edge(String name, Node origin, Node destination, int weight, String travelType) {
         this.name = name;
         this.origin = origin;
         this.destination = destination;
         this.weight = weight;
+        this.travelType = travelType;
     }
 
     public Node getDestination() {
@@ -25,6 +28,10 @@ public class Edge {
 
     public void setWeight(int newWeight) {
         weight = newWeight;
+    }
+
+    public String getTravelType() {
+        return travelType;
     }
 
     public String getName() {
