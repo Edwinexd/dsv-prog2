1102413d3b9bcd4c7aaf33d55d64448181641977
ShovelTime <gousaid67@gmail.com>
2023-03-28 11:09:54 +0200

    Wrote Edge and ListGraph boilerplate

--- src/main/java/com/gouswin/Edge.java
+++ src/main/java/com/gouswin/Edge.java
@@ -1,4 +1,31 @@
 package com.gouswin;
 
 public class Edge {
+
+    String name;
+    Node origin;
+    Node destination;
+    double weight;
+
+    public Node getDestination() {
+        return destination;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" +
+                "name='" + name + '\'' +
+                ", origin=" + origin +
+                ", destination=" + destination +
+                ", weight=" + weight +
+                '}';
+    }
 }
--- src/main/java/com/gouswin/ListGraph.java
+++ src/main/java/com/gouswin/ListGraph.java
@@ -1,4 +1,72 @@
 package com.gouswin;
 
-public class ListGraph {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class ListGraph{
+    public Node add(Node node)
+    {
+        return null;
+    }
+    public void remove(Node node) throws NoSuchElementException
+    {
+
+    }
+
+    public void connect() throws IllegalAccessException, IllegalStateException, NoSuchElementException
+    {
+
+    }
+
+    public void disconnect() throws NoSuchElementException, IllegalStateException
+    {
+
+    }
+
+    public void setConnectionWeight() throws NoSuchElementException, IllegalArgumentException
+    {
+
+    }
+
+    public ArrayList<Node> getNodes()
+    {
+        return null;
+    }
+
+    public Edge[] getEdgesfrom(Node node) throws NoSuchElementException
+    {
+        return null;
+    }
+
+    public Edge[] getEdgesBetween(Node nodeA, Node nodeB) throws NoSuchElementException
+    {
+        return null;
+    }
+
+    public boolean pathExists(Node from, Node to)
+    {
+        return false;
+    }
+
+
+    public String toString()
+    {
+        return "";
+    }
+
+
+    public  ArrayList<Edge> getPath(Node from, Node to) throws NoSuchElementException
+    {
+        return null;
+    }
+
+
+
+
+}
+
+class Node
+{
+
 }
